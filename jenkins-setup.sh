8eca7ecfb3ef12bb1829c6b4366e04db343e11f2
yogeshgunasekaran <106590073+yogeshgunasekaran@users.noreply.github.com>
2022-06-29 20:13:23 +0530

    Add files via upload

---
+++ jenkins-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Install Java
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+
+# Install the ca-certificates package, a tool which allows SSL-based applications to check for the authenticity of SSL connections
+sudo apt install ca-certificates -y
+
+# Install maven, git, wget and unzip
+sudo apt install maven git wget unzip -y
+
+# Add the framework repository key
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Add a Jenkins apt repository entry
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update local package index, then finally install Jenkins
+sudo apt-get update
+sudo apt-get install jenkins -y
+
