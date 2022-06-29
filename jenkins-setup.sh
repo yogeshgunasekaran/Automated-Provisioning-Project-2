9c7754e671698c9e2edd01c156beb37658d0f9a9
yogeshgunasekaran <106590073+yogeshgunasekaran@users.noreply.github.com>
2022-06-29 22:48:14 +0530

    Update jenkins-setup.sh

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -10,11 +10,11 @@ sudo apt install ca-certificates -y
 sudo apt install maven git wget unzip -y
 
 # Add the framework repository key
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 # Add a Jenkins apt repository entry
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
